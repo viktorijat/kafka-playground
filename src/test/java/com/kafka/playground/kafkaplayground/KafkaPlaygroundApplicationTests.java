e5ef97807f22678cc2205a687dfa244d275f6f30
Viktorija Tosheva <viktorija.tosheva@tipico.com>
2019-02-11 13:07:58 +0100

    first commit

---
+++ src/test/java/com/kafka/playground/kafkaplayground/KafkaPlaygroundApplicationTests.java
@@ -0,0 +1,17 @@
+package com.kafka.playground.kafkaplayground;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaPlaygroundApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
